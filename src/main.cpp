f677e3bfab72de85e3c38be7f61e76a4bb5e47d6
ThePigFarmer <alanwedel19@gmail.com>
2023-03-16 21:23:41 -0500

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -15,29 +15,29 @@ uint32_t prevMillis;
 const uint16_t timer1 = 1000;
 
 void setup() {
-    Serial.begin(MONITOR_SPEED);
-    Wire.begin();  // for DS3231
-    Serial.print("Serial and I2C started\n");
+  Serial.begin(MONITOR_SPEED);
+  Wire.begin(); // for DS3231
+  Serial.print("Serial and I2C started\n");
 
-    // loadValveData(); // not for testing
-}  // end setup
+  // loadValveData(); // not for testing
+} // end setup
 
 void loop() {
-    t = rtc.getTime();
-    uint16_t minutesSinceMidnight = t.hour * 60 + t.min;
+  t = rtc.getTime();
+  uint16_t minutesSinceMidnight = t.hour * 60 + t.min;
 
-    if ((millis() - prevMillis) > timer1) {
-        prevMillis = millis();
-        v.run(minutesSinceMidnight);
-    }  // end timed loop
+  if ((millis() - prevMillis) > timer1) {
+    prevMillis = millis();
+    v.run(minutesSinceMidnight);
+  } // end timed loop
 
-    bnt.read();
+  bnt.read();
 
-    if (bnt.changed()) {
-        // press for eeprom write
-        if (bnt.isPressed()) {
-            // v.putInEEPROM();
-            Serial.println(F("Saved valves to EEPROM"));
-        }
+  if (bnt.changed()) {
+    // press for eeprom write
+    if (bnt.isPressed()) {
+      // v.putInEEPROM();
+      Serial.println(F("Saved valves to EEPROM"));
     }
-}  // end main loop
+  }
+} // end main loop
--- src/valves.cpp
+++ src/valves.cpp
@@ -2,38 +2,26 @@
 
 #define between(num, min, max) (((min) <= (num)) && ((num) <= (max)))
 
-void valves::putInEEPROM()
-{
-    EEPROM.put(0, times);
-}
+void valves::putInEEPROM() { EEPROM.put(0, times); }
 
-void valves::begin()
-{
-    EEPROM.begin();
-}
+void valves::begin() { EEPROM.begin(); }
 
-void valves::getFromEEPROM()
-{
-    EEPROM.get(eeIndent, times);
-}
+void valves::getFromEEPROM() { EEPROM.get(eeIndent, times); }
 
-bool run(uint16_t minsSinceMidnight)
-{
-    bool x[4];
+bool run(uint16_t minsSinceMidnight) {
+  uint8_t x[4];
 
-    for (uint8_t thisValve = 0; thisValve < 4; thisValve++)
-    {
-        for (uint8_t i = 0; i < 4; i++)
-        {
-            if (between(minsSinceMidnight, times[thisValve][0][i], times[thisValve][1][i]))
-            {
-                x[thisValve] = true;
-                break;
-            }
-            else
-            {
-                x[thisValve] = false;
-            }
-        }
+  for (uint8_t thisValve = 0; thisValve < 4; thisValve++) {
+    for (uint8_t i = 0; i < 4; i++) {
+      if (between(minsSinceMidnight, times[thisValve][0][i],
+                  times[thisValve][1][i])) {
+        x[thisValve] = 1;
+        break;
+      } else {
+        x[thisValve] = 0;
+      }
     }
+  }
+
+  return x;
 }
