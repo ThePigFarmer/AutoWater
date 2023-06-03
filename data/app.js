4c22ddbb4469ca896242f864d4c6525f9c2ff2c3
ThePigFarmer <crgwedel@gmail.com>
2023-06-03 12:12:31 -0500

    it can tell what you clicked on

--- data/app.js
+++ data/app.js
@@ -1,12 +1,19 @@
-console.log("starting js...");
+console.log("starting app.js...");
 
-window.addEventListener("DOMContentLoaded", function() {
-    let boxes = document.querySelectorAll(".box");
+window.addEventListener("DOMContentLoaded", function () {
+    let boxes = document.querySelectorAll(".valve");
 
-    Array.from(boxes, function(box) {
+    Array.from(boxes, function (box) {
         box.addEventListener("click", function () {
-            alert(this.classList[1]);
-            console.log(this.classList[1]);
+            var valveNum = this.classList[1];
+            var valveNumInDay = this.id;
+            console.log("clicked: valve " + valveNum + ",   id " + valveNumInDay);
+            openDurationEntryForm(valveNum, valveNumInDay);
         });
     });
 });
+
+// Duration Entry Form
+function openDurationEntryForm(valveNum, valveNumInDay) {
+    alert(valveNum + " " + valveNumInDay);
+}
